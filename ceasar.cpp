3d030c49250dcf1174c8684870007698d48405fd
Tachas1 <115953112+Tachas1@users.noreply.github.com>
2022-10-16 23:00:54 +0200

    Add files via upload

---
+++ ceasar.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+void ceasar(char cipher[], char shift);
+
+int main() {
+	char text[] = "wp nsTQqcP op GTRPYPCP PDE FY DJDEPXP OP NSTQQCPXPYE azwjlwASLMPETBFP";
+	char shift = -128;
+
+	cout << text << endl;
+	ceasar(text, shift);
+	cout << text << endl;
+
+	return 0;
+}
+
+void ceasar(char cipher[], char shift) {
+	for (int i = 0; cipher[i] != '\0'; i++) {
+		if (cipher[i] >= 'a' && cipher[i] <= 'z') {
+			cipher[i] = (97 + (cipher[i] + shift - 97) % 26);
+			if(cipher[i] < 'a')
+				cipher[i] = cipher[i] + 'z' - 'a' + 1;
+		}
+		else if (cipher[i] >= 'A' && cipher[i] <= 'Z') {
+			cipher[i] = (65 + (cipher[i] + shift - 65) % 26);
+			if (cipher[i] < 'A')
+				cipher[i] = cipher[i] + 'Z' - 'A' + 1;
+		}
+	}
+}
